69e9af5c9c8eb64dfe5d894c613455239bd59993
Cdm2883 <Cdm2883@hotmail.com>
2025-01-20 23:48:01 +0800

    build: images/models assets

--- website/scripts/pack.js
+++ website/scripts/pack.js
@@ -1,3 +1,4 @@
+import fs from "node:fs";
 import { copy, exec, mkdirs } from "./utils.js";
 
 await exec("npm run clear");
@@ -7,3 +8,5 @@ mkdirs("build/dist");
 copy("src/index.html", "build/index.html");
 copy("src/dist/index.js", "build/dist/index.js");
 copy("src/dist/index.css", "build/dist/index.css");
+fs.cp("src/assets/images", "build/assets/images", { recursive: true }, () => {});
+fs.cp("src/assets/models", "build/assets/models", { recursive: true }, () => {});
