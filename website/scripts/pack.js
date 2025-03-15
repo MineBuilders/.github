9000051d7f39327c930e41cde19f55d118bb2c42
Cdm2883 <Cdm2883@hotmail.com>
2025-03-15 23:04:43 +0800

    build(website): add file hash

--- website/scripts/pack.js
+++ website/scripts/pack.js
@@ -1,13 +1,25 @@
 import fs from "node:fs";
+import crypto from "node:crypto";
 import { copy, exec, mkdirs } from "./utils.js";
 
 await exec("npm run clear");
 await exec("npm run build");
 
 mkdirs("build/dist");
-copy("src/index.html", "build/index.html");
-copy("src/dist/index.js", "build/dist/index.js");
-copy("src/dist/index.css", "build/dist/index.css");
-fs.cp("src/assets/images", "build/assets/images", { recursive: true }, () => {});
-fs.cp("src/assets/models", "build/assets/models", { recursive: true }, () => {});
-fs.cp("src/assets/fonts", "build/assets/fonts", { recursive: true }, () => {});
+
+const getHash = (path) => crypto.createHash('sha256')
+    .update(fs.readFileSync(path)).digest('hex').slice(0, 6);
+const [jsHash, cssHash] = ['js', 'css']
+    .map(ext => [getHash(`src/dist/index.${ext}`), ext])
+    .map(([hash, ext]) => `dist/index.${hash}.${ext}`);
+copy('src/dist/index.js', 'build/' + jsHash);
+copy('src/dist/index.css', 'build/' + cssHash);
+
+const html = fs.readFileSync('src/index.html', 'utf8')
+    .replace('dist/index.js', jsHash)
+    .replace('dist/index.css', cssHash);
+fs.writeFileSync('build/index.html', html);
+
+['images', 'models', 'fonts']
+    .map(dir => [`src/assets/${dir}`, `build/assets/${dir}`])
+    .forEach(dirs => fs.cpSync(dirs[0], dirs[1], { recursive: true }));
